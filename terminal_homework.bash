c72feaf0959f04b1ec91bc80ed10751c96a3b1e7
geno223 <149203931+geno223@users.noreply.github.com>
2023-11-14 11:04:40 -0500

    Update terminal_homework.bash

--- terminal_homework.bash
+++ terminal_homework.bash
@@ -1,29 +1,83 @@
-//Section 1
-//1. The acronym DRY stands for 'don't repeat yourself' and 
-// we should pay attention to avoid repeating code
+# Copy this text into your `.bash` homework file. Copy and paste the code you successfully used into terminal below each prompt
 
-// 2. The difference between var let and const is 
-// var and let both create variables that can be reassigned another value
-// const creates variables that cannot be reassigned another value
+# Open the **Terminal app**
+command space
+# Create a new directory on your desktop called `galaxy_far_far_away`and enter it
+mkdir galaxy_far_far_away
+# Create a directory called `death_star`
+mkdir death_star
+# and make the following files inside of it:
+# darth_vader.txt
+# princess_leia.txt
+# storm_trooper.txt
+cd death_star
+touch darth_vader.txt princess_leia.txt storm_trooper.txt
 
-//Section 3
-//1.the code below is an infite loop. 
-//the code is an infinite loop because
-//the condition in the while loop always evaluates to true
+# In `galaxy_far_far_away`, make a directory named `tatooine`
+cd first_homework; cd galaxy_far_far_away mkdir tatooine
+# and create the following files in it:
+# luke.txt
+# ben_kenobi.txt
+cd tatooine; touch luke.txt ben_kenobi.tx
 
-while (true) {
-	console.log('Do not run this loop');
-}
+# Inside of `tatooine` make a directory called `millenium_falcon`
+# and in it create:
+# han_solo.txt
+# chewbaca.txt
+mkdir millenium_falcon; cd millenium_falcon touch han_solo.txt chewbacca.txt
+# Rename `ben_kenobi.txt` to `obi_wan.txt
+mv ben_kenobi.txt obi_wan.txt
+mv: rename ben_kenobi.txt to obi_wan.txt: No such file or directory
+# Copy `storm_trooper.txt` from `death_star` to `tatooine`
+cd death_star; cp storm_trooper.txt tatooine
 
-// 2.I believe the code below is an infinite loopand I expect the output to be "true".
+# Move `luke.txt` and `obi_wan.txt` to the `millenium_falcon`
+mv luke.txt obi_wan.txt millenium_falcon
+mv: millenium_falcon is not a directory
 
-const runProgram = true;
+# Move `millenium_falcon` out of `tatooine` and into `galaxy_far_far_away`
+mv millenium_falcon galaxy_far_far_away
+# Move `millenium_falcon` into `death_star`
+mv millenium_falcon death_star
+# Move `princess_leia.txt` into the `millenium_falcon`
+ mv princess_leia.txt millenium_falcon
+# Delete `obi_wan.txt`
+rm obi_wan.txt
+# In `galaxy_far_far_away`, make a directory called `yavin_4`
+cd galaxy_far_far_away; mkdir yavin_4
+# Move the `millenium_falcon` out of the `death_star` and into `yavin_4`
+cd death_star; mv 
+# Make a directory in `yavin_4` called `x_wing`
 
-while (runProgram) {
-	console.log('Do not run this loop');
-	runProgram = false;
-}
+//(I'm getting stuck because my folders arent moving when i do mv command they just disappear.)
 
-//Section 4
-//1.for loop is used when the number of iterations is known
-//they're both used to execute the statements repeatedly while the program runs
+
+
+
+# Move `princess_leia.txt` to `yavin_4` and `luke.txt` to `x_wing`
+
+# Move the `millenium_falcon` and `x_wing` out of `yavin_4` and into `galaxy_far_far_away`
+
+# In `death_star`, create directories for `tie_fighter_1`, `tie_fighter_2` and `tie_fighter_3`
+
+# Move `darth_vader.txt` into `tie_fighter_1`
+
+# Make a copy of `storm_trooper.txt` in both `tie_fighter_2` and `tie_fighter_3`
+
+# Move all of the `tie_fighters` out of the `death_star` and into `galaxy_far_far_away`
+
+# Be careful with this command - cannot undo!
+
+# Make sure you delete the right thing, or you could accidentally delete the contents of your computer (it has happened).
+
+# This command will typically not ask you if you really want to delete. It will just delete so doublecheck you are deleting exactly what you mean to delete
+
+# Remove `tie_fighters` 2 and 3.
+
+# Touch a file in "**x_wing**" called "**the_force.txt**".
+
+# Destroy the "**death_star**" and anyone inside of it.
+
+# Return "**x_wing**" and the "**millenium_falcon**" to "**yavin_4**".
+
+# Celebrate!
