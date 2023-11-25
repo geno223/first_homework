c265fff23cc32e0e964322fb55e36a111fed6813
geno223 <149203931+geno223@users.noreply.github.com>
2023-11-25 13:13:44 -0500

    Update terminal_homework.bash

--- terminal_homework.bash
+++ terminal_homework.bash
@@ -8,123 +8,93 @@
 # darth_vader.txt
 # princess_leia.txt
 # storm_trooper.txt
-cd desktop; 
-mkdir galaxy_far_far_away2; 
-cd galaxy_far_far_away2
-mkdir death_star2
-cd death_star2;
-touch darthvader.txt princessleia.txt stormtrooper.txt
-
+cd desktop;
+mkdir galaxy;
+cd galaxy;
+mkdir death;
+cd death;
+touch darth.txt princess.txt trooper.txt
 # In `galaxy_far_far_away`, make a directory named `tatooine`
-
 # and create the following files in it:
 # luke.txt
 # ben_kenobi.txt
 cd ..;
-mkdir tatooine; 
-cd tatooine;
-touch luke2.txt benkenobi.txt
+mkdir tatoo;
+cd tatoo;
+touch luke.txt ben.txt;
+
 # Inside of `tatooine` make a directory called `millenium_falcon`
 # and in it create:
 # han_solo.txt
 # chewbaca.txt
-
-mkdir mill_falc;
-cd mill_falc;
-touch hansolo.txt chewy.txt
-
-
+mkdir falcon;
+cd falcon;
+touch solo.txt chewbaca.txt
 # Rename `ben_kenobi.txt` to `obi_wan.txt
 cd ..;
-mv benkenobi.txt obiwan.txt 
-
+cd tatoo;
+mv ben.txt obi.txt;
 # Copy `storm_trooper.txt` from `death_star` to `tatooine`
-
-cp stormtrooper.txt ../tatooine
-
+cd ..;
+cd death;
+cp trooper.txt ../tatoo
 # Move `luke.txt` and `obi_wan.txt` to the `millenium_falcon`
-mv luke2.txt obiwan.txt mill_fal
+mv luke.txt obi.txt falcon
 # Move `millenium_falcon` out of `tatooine` and into `galaxy_far_far_away`
-
+cd ..;
+mv falcon ..;
 
 # Move `millenium_falcon` into `death_star`
-
-
-I had trouble here i did "mv mill_falc galaxy_far_far_away2" and then i coldnt find the mill_falc directory
-
+cd ..;
+mv falcon death
 
 # Move `princess_leia.txt` into the `millenium_falcon`
-
 # Delete `obi_wan.txt`
+cd death;
+mv princess.txt falcon;
+cd falcon;
+rm obi.txt;
 
 # In `galaxy_far_far_away`, make a directory called `yavin_4`
-
-cd galaxy_far_far_away2;
-mkdir yavin4
-
 # Move the `millenium_falcon` out of the `death_star` and into `yavin_4`
-
-
-
-lost access to mill_falc
-
-
-
+cd galaxy;
+mkdir yavin4;
+cd death;
+mv falcon ../yavin4;
 # Make a directory in `yavin_4` called `x_wing`
-
-# Move `princess_leia.txt` to `yavin_4` and `luke.txt` to `x_wing`
-mkdir xwing;
- mv princessleia.txt ../yavin4;
-
-
- couldnt mve luke.txt becauase i lost access to mill_falc
-
-
+cd ..;
+cd yavin4;
+mkdir x_wing;
+# Move `princess_leia.txt` to `yavin_4` 
+cd falcon;
+mv princess.txt ..;
 
 # Move the `millenium_falcon` and `x_wing` out of `yavin_4` and into `galaxy_far_far_away`
-
-
-lost access to mill_falc
-
+cd ..;
+mv falcon ..;
+mv x_wing ..;
 # In `death_star`, create directories for `tie_fighter_1`, `tie_fighter_2` and `tie_fighter_3`
+cd death;
+ mkdir tf1 tf2 tf3  ;
 
-cd death_star2;
-mkdir tie_fighter tie_fighter2 tie_fighter3
 # Move `darth_vader.txt` into `tie_fighter_1`
-this was my response but it resulted in :mv: rename ../ to tie_fighter/../: Invalid argument
-mv darthvader.txt ../ tie_fighter    
-
-
-# Make a copy of `storm_trooper.txt` in both `tie_fighter_2` and `tie_fighter_3`
-this was my response but it resulted in: cp: ../ is a directory (not copied).
-cp: tie_fighter2 is a directory (not copied).
-
-cp stormtrooper.txt ../ tie_fighter2 tie_fighter3
+mv darth.txt tf1;
 # Move all of the `tie_fighters` out of the `death_star` and into `galaxy_far_far_away`
-
-this was my response but it resulted in:cp: tie_fighter is a directory (not copied).
-cp: tie_fighter2 is a directory (not copied).
-cp: tie_fighter3 is a directory (not copied).
-
-response: cp tie_fighter tie_fighter2 tie_fighter3 ..
-
+mv tf1 tf2 tf3 ..;
 
 # Be careful with this command - cannot undo!
+
 # Make sure you delete the right thing, or you could accidentally delete the contents of your computer (it has happened).
 # This command will typically not ask you if you really want to delete. It will just delete so doublecheck you are deleting exactly what you mean to delete
 # Remove `tie_fighters` 2 and 3.
-
-this was my response:rm tie_fighter2 tie_fighter3 
-
-result:rm: tie_fighter2: is a directory
-rm: tie_fighter3: is a directory
-
-
+rm tf2 tf3;
 # Touch a file in "**x_wing**" called "**the_force.txt**".
-cd ..;
-cd yavin4;
-touch the_force.txt
+cd x_wing;
+touch force.txt;
 # Destroy the "**death_star**" and anyone inside of it.
-rm -r death_star2
+cd galaxy;
+rm -r death;
 # Return "**x_wing**" and the "**millenium_falcon**" to "**yavin_4**".
+mv falcon yavin4;
+mv x_wing yavin4;
 # Celebrate!
